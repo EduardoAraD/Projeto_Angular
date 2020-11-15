97042a6266316d1ade7fdc98625677465e3eb32e
EduardoAraD <araujocarlos893@gmail.com>
2020-11-15 18:57:37 -0300

    add SnackBar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 import { Match } from '../model/match';
 
+import {
+  MatSnackBar
+} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'spa-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
   urlShieldHome: string;
   urlShieldAway: string;
   nameHome: string;
@@ -16,9 +20,9 @@ export class HomeComponent implements OnInit {
   golsHome: number;
   golsAway: number;
 
-  matchs: Match[] = []
+  matchs: Match[] = [];
 
-  constructor() { 
+  constructor(private _snackBar: MatSnackBar) { 
     this.urlShieldHome = "";
     this.urlShieldAway = "";
     this.nameHome = "";
@@ -29,15 +33,39 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  validadeValues(): boolean {
+    if(this.urlShieldHome === "") {
+      this.openSnackBar("Link do escudo do clube mandante é requerido.", "error_snackbar")
+      return false;
+    }
+    if(this.urlShieldAway === "") {
+      this.openSnackBar("Link do escudo do clube visitante é requerido.", "error_snackbar")
+      return false;
+    }
+    if(this.nameHome === "") {
+      this.openSnackBar("Nome do clube mandante é requerido.", "error_snackbar")
+      return false;
+    }
+    if(this.urlShieldAway === "") {
+      this.openSnackBar("Nome do clube visitante é requerido.", "error_snackbar")
+      return false;
+    }
+    if(this.golsHome <= -1 || this.golsAway <= -1) {
+      this.openSnackBar("Valor numérico inválido, somente positivos.", "error_snackbar")
+      return false;
+    }
+    
+    return true;
+  }
+
   createMatch(): void{
-    if(this.urlShieldHome === "") return;
-    if(this.urlShieldAway === "") return;
-    if(this.nameHome === "") return;
-    if(this.nameAway === "") return;
+    if(!this.validadeValues()) return;
 
     const match = new Match();
-    match.urlShieldHome = this.urlShieldHome;
-    match.urlShieldAway = this.urlShieldAway;
+    match.urlShieldHome = this.urlShieldHome === "botaagua.jpg" ?
+      "../../assets/botaagua.jpg" : this.urlShieldHome;
+    match.urlShieldAway = this.urlShieldAway  === "botaagua.jpg" ?
+      "../../assets/botaagua.jpg" : this.urlShieldAway;
     match.nameHome = this.nameHome;
     match.nameAway = this.nameAway;
     match.golsHome = this.golsHome;
@@ -45,12 +73,23 @@ export class HomeComponent implements OnInit {
 
     this.matchs.push(match)
 
+    this.openSnackBar("Partida Adicionada.", "done_snackbar")
+
     this.urlShieldHome = "";
     this.urlShieldAway = "";
     this.nameHome = "";
     this.nameAway = "";
     this.golsHome = 0;
     this.golsAway = 0;
+  }
+
+  openSnackBar(message: string, classStyle: string) {
+    this._snackBar.open(message, "", {
+      duration: 3000,
+      panelClass: [classStyle],
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+    });
   }
 
 }
